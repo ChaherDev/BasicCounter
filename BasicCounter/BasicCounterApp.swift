6dfc65d142ab31132fc0a761db31c733d09c2488
Chaher <chaher.croissance.public@gmail.com>
2023-11-21 08:56:27 +0100

    Initial Commit

---
+++ BasicCounter/BasicCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCounterApp.swift
+//  BasicCounter
+//
+//  Created by Chaher Machhour on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
